97e48cc37dfaa1a1e85def42def4d541d673109b
Artem Kovalevskyi <artem4852@MacBook-Pro--Artem.local>
2024-07-17 16:55:19 +0200

    worked on newsletter and footer, remade hero, small improvements

--- static/js/home_script.js
+++ static/js/home_script.js
@@ -1,34 +1,47 @@
-scrolling = []
-strength = 1.5
-for (let i = 0; i < 8; i++) {
-    if (scrolling[i-1] > 0) {
-        scrolling.push(-Math.random() * strength);
-    } else {
-        scrolling.push(Math.random() * strength);
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const imagecols = document.querySelectorAll('.imagecol');
+    const imgheight = imagecols[0].children[0].getBoundingClientRect().height;
+    let scrolling = [];
+
     for (let i = 0; i < imagecols.length; i++) {
-        pos = Math.random() * 800 - 400;
+        scrolling.push(-1);
+    }
+
+    starts = []
+
+    for (let i = 0; i < imagecols.length; i++) {
+        pos = Math.random() * 2*imgheight - imgheight;
+        starts.push(pos);
         imagecols[i].style.top = pos + 'px';
         console.log(imagecols[i].style.top);
     }
-});
 
-let prev = window.scrollY;
+    setInterval(() => {
+        for (let i = 0; i < imagecols.length; i++) {
+            const col = imagecols[i];
+            const images = col.children;
 
-window.addEventListener('scroll', () => {
-    const imagecols = document.querySelectorAll('.imagecol');
-    for (let i = 0; i < imagecols.length; i++) {
-        pos = parseFloat(imagecols[i].style.top.replace('px', ''));
-        pos += scrolling[i] * (window.scrollY - prev);
-        imagecols[i].style.top = pos + 'px';
+            console.log(col.getBoundingClientRect().top);
+            if (col.getBoundingClientRect().top <= starts[i]-imgheight) {
+                col.appendChild(images[0]);
+                col.style.top = parseFloat(col.style.top.replace('px', '')) + imgheight + 'px';
+            }
 
-        if (i+1 == imagecols.length) {
-            console.log(imagecols[i].style.top);
+            col.style.top = parseFloat(col.style.top.replace('px', '')) + scrolling[i]*2 + 'px';
         }
-    }
-    prev = window.scrollY;
+    }, 30);
 });
+
+// window.addEventListener('scroll', () => {
+//     const imagecols = document.querySelectorAll('.imagecol');
+//     for (let i = 0; i < imagecols.length; i++) {
+//         pos = parseFloat(imagecols[i].style.top.replace('px', ''));
+//         pos += scrolling[i] * (window.scrollY - prev);
+//         imagecols[i].style.top = pos + 'px';
+
+//         if (i+1 == imagecols.length) {
+//             console.log(imagecols[i].style.top);
+//         }
+//     }
+//     prev = window.scrollY;
+// });
